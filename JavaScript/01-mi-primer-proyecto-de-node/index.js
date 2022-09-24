592f2043ce45744f87956d6d5c1a1d99fe2318dc
Mariano Chiodi <chiodimar@gmail.com>
2022-09-24 18:31:12 -0300

    subiendo ej2 de JS

--- JavaScript/01-mi-primer-proyecto-de-node/index.js
+++
@@ -1,2 +0,0 @@
-// ESTE ES EL MENSAJE DE BIENVENIDA
-console.log("Esta es la puerta de entrada al proyecto");
---
+++ JavaScript/02-Ejercicio/index.js
@@ -0,0 +1,6 @@
+const persona = [ "Mariano", 37, true, fecha, ["Amor en tiempos de colera","Gabo Marquez",1985,"https://es.wikipedia.org/wiki/El_amor_en_los_tiempos_del_c%C3%B3lera" ]];
+console.log(persona);
+
+
+var fecha = new Date("february 26 1985");
+console.log(fecha);
