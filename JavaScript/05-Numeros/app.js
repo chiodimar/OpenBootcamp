fa594d089a2bf1d8c0a2703d43018a35ec5e01d4
Mariano Chiodi <chiodimar@gmail.com>
2022-10-03 16:20:29 -0300

    Subiendo Desafio 5 - Numeros

---
+++ JavaScript/05-Numeros/app.js
@@ -0,0 +1,18 @@
+let alturaCm = 190;
+console.log(parseInt(alturaCm));
+
+let alturaMts = 1.9;
+console.log(parseFloat(alturaMts));
+
+let pesoKg = 83.2;      
+console.log(parseFloat(pesoKg));
+
+let alturaMtsArriba = 1.88;
+console.log(alturaMtsArriba.toFixed(1));
+
+let pesoKgAbajo = 83.2;
+console.log(pesoKgAbajo.toFixed());
+
+let maximoFrase = "el máximo valor que se puede obtener en Javascript + 1";
+let maximoTotal = Number.MAX_VALUE;
+console.log(maximoFrase == maximoTotal);
