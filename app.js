312859e5fa331e15212f56bae22ebfdfe2829446
Mariano Chiodi <chiodimar@gmail.com>
2022-10-24 22:33:51 -0300

    Ejercicio 1 Modulo 7

---
+++ app.js
@@ -0,0 +1,8 @@
+const familia = ["Humberto","Ana Maria","Aldana","Araceli","Agostina","Mariano"];
+const SetFamilia = new Set(familia);
+
+console.log(SetFamilia);
+SetFamilia.add("Mariano");
+console.log(SetFamilia);
+SetFamilia.add("JavaScript");
+console.log(SetFamilia);
